53e9a0160dc513b573f7418c330831cfae4f909d
deb1704-student <dasdebanjan.d2006@gmail.com>
2026-08-22 13:49:43 +0530

    Time: 5 ms (31.76%) | Memory: 42.9 MB (78.97%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+        int[] dp=new int[n];
+
+        for(int i=0;i<n;i++){
+            dp[i]=piles[i];
+        }
+
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j = i+len-1;
+                dp[i]=Math.max(piles[i]-dp[i+1],piles[j]-dp[i]);
+            }
+        }
+        return dp[0]>0;
+
     }
 }
