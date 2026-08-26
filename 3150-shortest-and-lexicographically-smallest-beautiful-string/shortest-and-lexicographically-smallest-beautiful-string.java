200907eb007ef1b9c1063ba35da74c896c13a347
deb1704-student <dasdebanjan.d2006@gmail.com>
2026-08-26 22:49:06 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (28.86%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int l=0, count =0;
+        String ans="";
+        for (int r=0;r<s.length();r++){
+            if(s.charAt(r)=='1'){
+                count++;
+            }
+            while(count==k){
+                String curr = s.substring(l,r+1);
+
+                if(ans.isEmpty()||ans.length()>curr.length()|| (ans.length()==curr.length() && curr.compareTo(ans)<0) ){
+                    ans=curr;
+                }
+
+                if(s.charAt(l)=='1'){
+                    count--;
+                }
+                l++;
+            }
+        }
+        return ans;
+        
+    }
+}
